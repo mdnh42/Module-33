5d69b535db39e8a56f316274ab6b375668aa3557
mdnh42 <mdnh.42@gmail.com>
2021-08-28 09:15:33 +0600

    js

--- food.js
+++ food.js
@@ -3,7 +3,11 @@ const searchFood = () =>{
     const searchText = searchField.value;
     console.log(searchText);
 
+    
+
     searchField.value = '';
+
+
 
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
 
@@ -15,6 +19,8 @@ const searchFood = () =>{
 
 const displySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+   
     meals.forEach(meal => {
         const div = document.createElement('div');
         div.classList.add('col');
@@ -28,16 +34,34 @@ const displySearchResult = meals => {
         </div>
         `
         searchResult.appendChild(div);
+    
     })
 }
 
-const loadMealDetail = mealId =>{
-    const url = `www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displyaMealDetails(data.meals[0]));
+const loadMealDetail = async mealId =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displyaMealDetails(data.meals[0]));
+    // fetch(url)
+    // .then(res => res.json())
+    // .then(data => displyaMealDetails(data.meals[0]));
 }
 
 const displyaMealDetails = meal =>{
-    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <div class="card bg-dark text-white">
+            <img src="${meal.strMealThumb}" class="card-img" alt="...">
+            <div class="card-img-overlay">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+            </div>
+        </div>
+    `;
+    mealDetails.appendChild(div);
+
 }
